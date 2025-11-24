9f17d8c24e40fac4a0b65a238ba0b11fc0104906
kuaya1 <Workkuaya@gmail.com>
2025-11-23 23:06:54 -0500

    style: Increase hero image size to match viewport frame

--- app/page.tsx
+++ app/page.tsx
@@ -50,7 +50,7 @@ export default function Home() {
                 <img
                   src="/images/hero-image.jpg"
                   alt="The Master Craftsman hand-lasting a boot"
-                  className="w-1/2 h-auto object-cover"
+                  className="w-4/5 h-3/4 object-contain"
                 />
               </div>
 
